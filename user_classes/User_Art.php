beb1d55a6a17de441564db601fdeb67bee7c0587
Bobby <bobmitch@gmail.com>
2022-09-01 03:05:09 +0000

    feat: anchor wrap grid items

--- user_classes/User_Art.php
+++ user_classes/User_Art.php
@@ -7,10 +7,12 @@ class User_Art {
 	public static function render_art_grid_item($art) {
 		?>
 		<div class="artgrid_item">
+			<a href='<?=Config::$uripath;?>/my-work/<?=$art->alias;?>'>
 			<?php 
 				$art_image = new Image($art->f_artthumbnail);
 				$art_image->render(300,'artgrid_image');
 			?>
+			</a>
 		</div>
 		<?php
 	}
