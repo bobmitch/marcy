ef22e67c307ae44c5c5781ccba42583ac3d60d8f
Bobby <bobmitch@gmail.com>
2022-09-01 03:56:21 +0000

    feat: start single art view

--- controllers/art/views/single-art/model.php
+++ controllers/art/views/single-art/model.php
@@ -2,6 +2,11 @@
 defined('CMSPATH') or die; // prevent unauthorized access
 
 $alias = CMS::Instance()->uri_segments[0];
-$art = new Content('art');
-$art->load_from_alias($alias);
+/* $art = new Content('art');
+$art->load_from_alias($alias); */
 
+$art_id = DB::fetch('select id from content where alias=? and content_type=2',$alias)->id;
+$art = Content::get_all_content_for_id($art_id);
+
+$image_id = $art->f_artthumbnail;
+$image = new Image($image_id);
--- controllers/art/views/single-art/view.php
+++ controllers/art/views/single-art/view.php
@@ -1,4 +1,17 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
+?>
+
+<section class='container'>
+    <h1><?=$art->title;?></h1>
+    <div class='fullimage'>
+        <?php $image->render('web','fullimage');?>
+    </div>
+</section>
+
+
+
+
+<?php
 CMS::pprint_r ($art);
 ?>
