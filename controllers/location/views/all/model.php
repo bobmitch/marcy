4621c759bb790815a95a1c8b61887fdf587a24e6
Bobby <bobmitch@gmail.com>
2022-09-01 22:15:25 +0000

    fix: only show listed locations

--- controllers/location/views/all/model.php
+++ controllers/location/views/all/model.php
@@ -4,13 +4,15 @@ defined('CMSPATH') or die; // prevent unauthorized access
 $content_search = new Content_Search();
 $content_search->type_filter = 'location';
 $content_search->published_only = true;
-$content_search->list_fields = ['locationaddress','locationcity','locationstate'];
+$content_search->list_fields = ['locationaddress','locationcity','locationstate','listlocation'];
 // only return locations that are listable
 $content_search->filters = array('f_listlocation'=>'1');
 
 $locations = $content_search->exec();
 
 $intro = Content::get_all_content_for_id(16); // Locations Introduction content item
+
+//CMS::pprint_r ($locations);
 
 ?>
 
