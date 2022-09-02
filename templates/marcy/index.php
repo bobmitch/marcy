c53aa97639d3fe2096b73ed729a28ae1dfc65e1c
Bobby <bobmitch@gmail.com>
2022-09-02 14:11:21 +0000

    feat: footers in social

--- templates/marcy/index.php
+++ templates/marcy/index.php
@@ -32,9 +32,30 @@
 	<?php CMS::Instance()->render_widgets('After Content');?>
 	
 	<footer data-theme="dark">
-		<div class='container'>
+		<section class="container">
+			<ul class='flex' id='socials'>
+				<li>
+					<a target="_blank" class='hasicon' href="https://www.facebook.com/marcymitchellart">
+						<span class='hidden'>Facebook</span>
+						<svg role="presentation" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" class='hasicon' href="https://www.instagram.com/marcymitchellart/?hl=en">
+						<span class='hidden'>Instagram</span>
+						<svg role="presentation" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://linktr.ee/marcymitchellart">
+						LinkTree
+					</a>
+				</li>
+			</ul>
+		</section>
+		<section class='container'>
 			<p>All content copyright &copy;2022 - Marcy Mitchell. All rights reserved.</p>
-		</div>
+		</section>
 		<?php CMS::Instance()->render_widgets('Footer');?>
 	</footer>
 
