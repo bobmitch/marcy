6b8988f457dfb23a90bdc214188cf70b0122bf32
Bobby <bobmitch@gmail.com>
2022-09-18 15:40:53 -0400

    fix: honor light dark mode

--- templates/marcy/index.php
+++ templates/marcy/index.php
@@ -3,7 +3,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en" data-theme="light">
+<html lang="en" data-theme="none">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
